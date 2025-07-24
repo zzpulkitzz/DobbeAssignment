2e9a5c84ae9f378b2f810f19492b84bd52dd31d4
zzpulkitzz <gpulkitgupta72@gmail.com>
2025-07-24 23:07:44 +0530

    test

--- FastAPI_backend/start.sh
+++ FastAPI_backend/start.sh
@@ -7,6 +7,7 @@ echo "PWD: $(pwd)"
 echo "Files:"
 ls -al
 
-echo "Starting MCP server..."
-mcp run Server/server.py --transport=sse > mcp.log 
+
+echo "Starting FastAPI server..."
+uvicorn gemini_api:app --host 0.0.0.0 
 
