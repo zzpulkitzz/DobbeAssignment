ff8ead2b74c3bb14547b1157b95552b762e09c93
zzpulkitzz <gpulkitgupta72@gmail.com>
2025-07-25 15:56:43 +0530

    test

--- FastAPI_backend/Server/server.py
+++ FastAPI_backend/Server/server.py
@@ -3,7 +3,7 @@ from database import engine
 from Server.base import Base
 import url  # This ensures MCP tools are registered
 import os
-mcp = FastMCP("DoctorScheduler", description="Doctor appointment and reporting tools")
+mcp = FastMCP("DoctorScheduler", description="Doctor appointment and reporting tools",host="0.0.0.0")
 
 url.register_tools(mcp)
 Base.metadata.create_all(engine)
@@ -11,5 +11,6 @@ Base.metadata.create_all(engine)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Render sets PORT automatically
-    mcp.run(host="0.0.0.0", port=port,transport="streamable-http")
+    mcp.run(
+        transport="streamable-http"
+    )
