a4dafad528fd34d3568d8b413eec3ceababa4fe1
zzpulkitzz <gpulkitgupta72@gmail.com>
2025-07-28 09:54:41 +0530

    test

--- FastAPI_backend/Server/server.py
+++ FastAPI_backend/Server/server.py
@@ -3,7 +3,7 @@ from database import engine
 from Server.base import Base
 import url  # This ensures MCP tools are registered
 import os
-mcp = FastMCP("DoctorScheduler", description="Doctor appointment and reporting tools",host="0.0.0.0")
+mcp = FastMCP("DoctorScheduler", description="Doctor appointment and reporting tools")
 
 url.register_tools(mcp)
 Base.metadata.create_all(engine)
