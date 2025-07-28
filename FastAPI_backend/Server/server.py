35772dbdf18a6f352e4571f2cbc62793d1c40628
zzpulkitzz <gpulkitgupta72@gmail.com>
2025-07-28 14:34:08 +0530

    test

--- FastAPI_backend/Server/server.py
+++ FastAPI_backend/Server/server.py
@@ -1,13 +1,15 @@
 from mcp.server.fastmcp import FastMCP
+from sqlalchemy import inspect
 from database import engine
 from Server.base import Base
-import url  # This ensures MCP tools are registered
-import os
+import url  
 mcp = FastMCP("DoctorScheduler", description="Doctor appointment and reporting tools")
 
 url.register_tools(mcp)
 Base.metadata.create_all(engine)
 
+inspector = inspect(engine)
+print(inspector.get_table_names())
 
 
 if __name__ == "__main__":
